8e81e25ab5162be6a21cbe70b276c8a24671deb0
CrispyMalex <50028598+malex5335@users.noreply.github.com>
2020-09-22 21:27:19 +0200

    removed static variables

--- src/main/java/de/riagade/BotInjector.java
+++ src/main/java/de/riagade/BotInjector.java
@@ -9,13 +9,9 @@ import java.util.Locale;
 public class BotInjector {
 
     public static void main(String[] args) {
-        //FabiAlertDiscordBot fadb = new FabiAlertDiscordBot(System.getenv().get("BOT_TOKEN"),
-        //        getLongFromString(System.getenv().get("GUILD_ID")),
-        //        getLongFromString(System.getenv().get("CHANNEL_ID")),
-        //        Locale.GERMANY);
-        FabiAlertDiscordBot fadb = new FabiAlertDiscordBot("NzEzMzg0MzQyOTQ4NDEzNTMx.XsfU8Q.EktCCZf0NqrWRBjeHM6POEmYrdY",
-                487181512844771331L,
-                487190167665508362L,
+        FabiAlertDiscordBot fadb = new FabiAlertDiscordBot(System.getenv().get("BOT_TOKEN"),
+                getLongFromString(System.getenv().get("GUILD_ID")),
+                getLongFromString(System.getenv().get("CHANNEL_ID")),
                 Locale.GERMANY);
     }
 
