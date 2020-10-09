9614e83c59495359e702fc3cac7236671385264f
Ikasumi-o <72375069+Ikasumi-o@users.noreply.github.com>
2020-10-09 16:57:25 +0200

    Update GuildBallTask.java

--- src/main/java/de/riagade/bot/task/timed/GuildBallTask.java
+++ src/main/java/de/riagade/bot/task/timed/GuildBallTask.java
@@ -5,7 +5,7 @@ import de.riagade.bot.task.feedback.RacoonTask;
 import de.riagade.genericDiscordBot.A_FeedbackTask;
 import de.riagade.genericDiscordBot.A_TimedTask;
 
-public class DailyRacoonTask extends A_TimedTask {
+public class GuildBallTask extends A_TimedTask {
     /**
      * @param cronExpression the cron expression to calculate the next execution
      */
