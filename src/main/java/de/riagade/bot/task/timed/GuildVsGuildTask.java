40c7c96fdbeadaa733e4a79e84d3b6cae89cf432
Ikasumi-o <72375069+Ikasumi-o@users.noreply.github.com>
2020-10-09 17:22:53 +0200

    Create GuildVsGuildTask.java

---
+++ src/main/java/de/riagade/bot/task/timed/GuildVsGuildTask.java
@@ -0,0 +1,20 @@
+package de.riagade.bot.task.timed;
+
+import de.riagade.bot.FabiAlertDiscordBot;
+import de.riagade.bot.task.feedback.GuildVsGuildTask;
+import de.riagade.genericDiscordBot.A_FeedbackTask;
+import de.riagade.genericDiscordBot.A_TimedTask;
+
+public class GuildVsGuildTask extends A_TimedTask {
+    /**
+     * @param cronExpression the cron expression to calculate the next execution
+     */
+    public GuildVsGuildTask(FabiAlertDiscordBot bot, String cronExpression) {
+        super(bot, cronExpression);
+    }
+
+    @Override
+    public A_FeedbackTask createFeedbackTask() {
+        return new GuildVsGuildTask((FabiAlertDiscordBot) getBot());
+    }
+}
