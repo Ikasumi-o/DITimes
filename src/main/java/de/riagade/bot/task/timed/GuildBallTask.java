3d3d1fd8dd77f42138a5ec5ca6ee317692a96c17
Ikasumi-o <72375069+Ikasumi-o@users.noreply.github.com>
2020-10-09 21:47:08 +0200

    Update GuildBallTask.java

--- src/main/java/de/riagade/bot/task/timed/GuildBallTask.java
+++ src/main/java/de/riagade/bot/task/timed/GuildBallTask.java
@@ -15,6 +15,6 @@ public class GuildBallTask extends A_TimedTask {
 
     @Override
     public A_FeedbackTask createFeedbackTask() {
-        return new RacoonTask((FabiAlertDiscordBot) getBot());
+        return new GuildBallTask((FabiAlertDiscordBot) getBot());
     }
 }
