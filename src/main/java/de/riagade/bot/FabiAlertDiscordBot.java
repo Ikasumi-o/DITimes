a1531f8e1dd21890cfbce303b0423db2f3a3749e
Ikasumi-o <72375069+Ikasumi-o@users.noreply.github.com>
2020-10-10 19:54:55 +0200

    Update FabiAlertDiscordBot.java

--- src/main/java/de/riagade/bot/FabiAlertDiscordBot.java
+++ src/main/java/de/riagade/bot/FabiAlertDiscordBot.java
@@ -29,7 +29,7 @@ public class FabiAlertDiscordBot extends A_TimedBot {
     @Override
     public List<A_TimedTask> getTasksList() {
         List<A_TimedTask> tasks = new ArrayList<>();
-        tasks.add(new DailyRacoonTask(this, "0 40 19 ? * * *"));
+        tasks.add(new DailyRacoonTask(this, "0 40 17 ? * * *"));
         tasks.add(new DailyWorldBossTask(this, "0 20 10,14,22 ? * * *"));
         tasks.add(new MoWedFriSoGuildBallTask(this, "0 20 18 ? * SUN,MON,WED,FRI *"));
 	tasks.add(new TueThuGuildVsGuildTask(this, "0 50 18 ? * TUE,THU *"));
