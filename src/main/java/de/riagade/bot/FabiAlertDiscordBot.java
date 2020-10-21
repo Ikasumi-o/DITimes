c1a8f8123cc2611d9b5b295f05ae0993782c28ce
Ikasumi-o <72375069+Ikasumi-o@users.noreply.github.com>
2020-10-21 16:21:50 +0200

    Update FabiAlertDiscordBot.java

--- src/main/java/de/riagade/bot/FabiAlertDiscordBot.java
+++ src/main/java/de/riagade/bot/FabiAlertDiscordBot.java
@@ -9,6 +9,11 @@ import de.riagade.bot.task.timed.WeeklyOverlordTask;
 import de.riagade.bot.task.timed.TueDragonQuestTask;
 import de.riagade.bot.task.timed.DailieEventsOverviewMoTask;
 import de.riagade.bot.task.timed.DailieEventsOverviewTueTask;
+import de.riagade.bot.task.timed.DailieEventsOverviewWedTask;
+import de.riagade.bot.task.timed.DailieEventsOverviewThuTask;
+import de.riagade.bot.task.timed.DailieEventsOverviewFriTask;
+import de.riagade.bot.task.timed.DailieEventsOverviewSatTask;
+import de.riagade.bot.task.timed.DailieEventsOverviewSunTask;
 import de.riagade.genericDiscordBot.A_TimedBot;
 import de.riagade.genericDiscordBot.A_TimedTask;
 
@@ -41,6 +46,11 @@ public class FabiAlertDiscordBot extends A_TimedBot {
 	tasks.add(new TueDragonQuestTask(this, "0 50 17 ? * TUE *"));
 	tasks.add(new DailieEventsOverviewMoTask(this, "0 0 8 ? * MON *")); 
 	tasks.add(new DailieEventsOverviewTueTask(this, "0 0 8 ? * TUE *")); 
+	tasks.add(new DailieEventsOverviewWedTask(this, "0 0 8 ? * WED *"));
+	tasks.add(new DailieEventsOverviewThuTask(this, "0 0 8 ? * THU *"));
+	tasks.add(new DailieEventsOverviewFriTask(this, "0 0 8 ? * FRI *"));
+	tasks.add(new DailieEventsOverviewSatTask(this, "0 0 8 ? * SAT *"));
+	tasks.add(new DailieEventsOverviewSunTask(this, "0 0 8 ? * SUN *"));
         return tasks;
     }
 }
