cfb4a827ae80cf77af304ce9d5a3c79fcb36bca3
Ikasumi-o <72375069+Ikasumi-o@users.noreply.github.com>
2020-10-27 02:49:05 +0100

    Add files via upload

---
+++ src/main/java/de/riagade/bot/task/feedback/SkyCastleBossTask.java
@@ -0,0 +1,30 @@
+package de.riagade.bot.task.feedback;
+
+import de.riagade.bot.FabiAlertDiscordBot;
+import de.riagade.bot.util.ResourcesHelper;
+import de.riagade.genericDiscordBot.A_FeedbackTask;
+
+public class SkyCastleBossTask extends A_FeedbackTask {
+    private FabiAlertDiscordBot bot;
+
+    public SkyCastleBossTask(FabiAlertDiscordBot bot){
+        setBot(bot);
+    }
+
+    /**
+     * @return the current {@link FabiAlertDiscordBot}
+     */
+    public FabiAlertDiscordBot getBot() {
+        return bot;
+    }
+
+    private void setBot(FabiAlertDiscordBot bot) {
+        this.bot = bot;
+    }
+
+    @Override
+    public void runLogic() {
+        getBot().sendMessage(ResourcesHelper.getResourceFileContent(getClass().getClassLoader(),
+                "SkyCastleBossMessage.txt"));
+    }
+}
