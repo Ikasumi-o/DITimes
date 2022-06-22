16b6a48469392653126bdcf20774a735d9d8b419
Ikasumi-o <72375069+Ikasumi-o@users.noreply.github.com>
2022-06-22 12:20:17 +0200

    Update FabiAlertDiscordBot.java

--- src/main/java/de/riagade/bot/FabiAlertDiscordBot.java
+++ src/main/java/de/riagade/bot/FabiAlertDiscordBot.java
@@ -1,19 +1,6 @@
 package de.riagade.bot;
 
 import de.riagade.bot.task.timed.DailyRacoonTask;
-import de.riagade.bot.task.timed.DailyWorldBossTask;
-import de.riagade.bot.task.timed.DailieEventsOverviewMoTask;
-import de.riagade.bot.task.timed.DailieEventsOverviewTueTask;
-import de.riagade.bot.task.timed.DailieEventsOverviewWedTask;
-import de.riagade.bot.task.timed.DailieEventsOverviewThuTask;
-import de.riagade.bot.task.timed.DailieEventsOverviewFriTask;
-import de.riagade.bot.task.timed.DailieEventsOverviewSatTask;
-import de.riagade.bot.task.timed.DailieEventsOverviewSunTask;
-import de.riagade.bot.task.timed.MoWedFriSoGuildBallTask;
-import de.riagade.bot.task.timed.TueThuGuildVsGuildTask;
-import de.riagade.bot.task.timed.FrSaRuinsBossTask;
-import de.riagade.bot.task.timed.WeeklyOverlordTask;
-import de.riagade.bot.task.timed.TueDragonQuestTask;
 import de.riagade.genericDiscordBot.A_TimedBot;
 import de.riagade.genericDiscordBot.A_TimedTask;
 
@@ -37,19 +24,6 @@ public class FabiAlertDiscordBot extends A_TimedBot {
     public List<A_TimedTask> getTasksList() {
         List<A_TimedTask> tasks = new ArrayList<>();
         tasks.add(new DailyRacoonTask(this, "0 40 17 ? * * *"));
-        tasks.add(new DailyWorldBossTask(this, "0 20 10,14,20 ? * * *"));
-	tasks.add(new DailieEventsOverviewMoTask(this, "0 0 9 ? * MON *")); 
-	tasks.add(new DailieEventsOverviewTueTask(this, "0 0 9 ? * TUE *")); 
-	tasks.add(new DailieEventsOverviewWedTask(this, "0 0 9 ? * WED *"));
-	tasks.add(new DailieEventsOverviewThuTask(this, "0 0 9 ? * THU *"));
-	tasks.add(new DailieEventsOverviewFriTask(this, "0 0 9 ? * FRI *"));
-	tasks.add(new DailieEventsOverviewSatTask(this, "0 0 9 ? * SAT *"));
-	tasks.add(new DailieEventsOverviewSunTask(this, "0 0 9 ? * SUN *"));
-	tasks.add(new MoWedFriSoGuildBallTask(this, "0 20 18 ? * SUN,MON,WED,FRI *"));
-	tasks.add(new TueThuGuildVsGuildTask(this, "0 50 18 ? * TUE,THU *"));
-	tasks.add(new FrSaRuinsBossTask(this, "0 50 12,20 ? * FRI,SAT *"));
-        tasks.add(new WeeklyOverlordTask(this, "0 50 18 ? * FRI *"));
-	tasks.add(new TueDragonQuestTask(this, "0 50 17 ? * TUE *"));
         return tasks;
     }
 }
