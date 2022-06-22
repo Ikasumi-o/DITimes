56c80ee1717c909ab9438a9dc520da1717ed63a6
Ikasumi-o <72375069+Ikasumi-o@users.noreply.github.com>
2022-06-22 12:48:38 +0200

    Update and rename DailieEventsOverviewFriTask.java to DailyDiabloBattlegroundTask.java

--- src/main/java/de/riagade/bot/task/timed/DailyDiabloBattlegroundTask.java
+++ src/main/java/de/riagade/bot/task/timed/DailyDiabloBattlegroundTask.java
@@ -1,20 +1,20 @@
 package de.riagade.bot.task.timed;
 
 import de.riagade.bot.FabiAlertDiscordBot;
-import de.riagade.bot.task.feedback.FriTask;
+import de.riagade.bot.task.feedback.DiabloBGTask;
 import de.riagade.genericDiscordBot.A_FeedbackTask;
 import de.riagade.genericDiscordBot.A_TimedTask;
 
-public class DailieEventsOverviewFriTask extends A_TimedTask {
+public class DailyDiabloBattlegroundTask extends A_TimedTask {
     /**
      * @param cronExpression the cron expression to calculate the next execution
      */
-    public DailieEventsOverviewFriTask(FabiAlertDiscordBot bot, String cronExpression) {
+    public DailyDiabloBattlegroundTask(FabiAlertDiscordBot bot, String cronExpression) {
         super(bot, cronExpression);
     }
 
     @Override
     public A_FeedbackTask createFeedbackTask() {
-        return new FriTask((FabiAlertDiscordBot) getBot());
+        return new DiabloBGTask((FabiAlertDiscordBot) getBot());
     }
 }
